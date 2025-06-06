f3792350f818549b7458d57163f522b8f0f2df55
Riccardoengin01 <riccardoengin@gmail.com>
2025-06-06 14:13:02 +0200

    Ensure data dir creation

--- lost_and_found/utils.py
+++ lost_and_found/utils.py
@@ -33,6 +33,7 @@ def _load_json(path):
 
 
 def _save_json(path, data):
+    os.makedirs(os.path.dirname(path), exist_ok=True)
     with open(path, 'w', encoding='utf-8') as f:
         json.dump(data, f, indent=2, ensure_ascii=False)
 
